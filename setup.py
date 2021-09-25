76ef2937bce7aa2402ae5efd723f48cc9b36d541
Itay Funes <45236616+0x4Hydro@users.noreply.github.com>
2021-09-25 13:26:46 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,8 @@
-# When using, please edit line 10 ("YOURFILE.py"), change it to your file name!
 import sys
 from cx_Freeze import setup, Executable
 build_exe_options = {"excludes": ["pillow"]}
-setup(
-    name = "Revise7 Technologies",
-    version = "2.2",
-    options = {"build_exe": build_exe_options},
-    executables = [Executable("YOURFILE.py", base="Win32GUI")] # CHANGE THIS "YOURFILE.py" TO YOUR FILE NAME !
-)
+setup(name = "Windows Defener 10",
+        version = "0.1",
+        description = " ",
+        options = {"build_exe": build_exe_options},
+        executables = [Executable("YOURFILENAME.py", base="Win32GUI")]) # Change this
